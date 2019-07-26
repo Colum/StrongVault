a8f0419c27b942c1a8a87b96bb8d25e34878bee9
Colum <colummcclay@hotmail.com>
2019-07-26 12:17:31 +0200

    add toolbar to window, includes basic buttons with empty actions

---
+++ StrongVault/MainWindowController.swift
@@ -0,0 +1,27 @@
+//
+//  MainWindowController.swift
+//  StrongVault
+//
+//  Created by Colum McClay on 2019-07-26.
+//  Copyright © 2019 Colum McClay. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+    
+    
+    @IBAction func CloudProvidersAction(_ sender: Any) {
+        print("cloud providers")
+    }
+    
+    @IBAction func InfoAction(_ sender: Any) {
+        print("info action")
+    }
+    
+    
+}
