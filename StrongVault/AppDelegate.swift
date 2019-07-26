b28b7bac048eca6ceeaf72477417a89b37c2d9b1
Colum <colummcclay@hotmail.com>
2019-07-26 11:18:27 +0200

    Initial Commit

---
+++ StrongVault/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StrongVault
+//
+//  Created by Colum McClay on 2019-07-26.
+//  Copyright © 2019 Colum McClay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
